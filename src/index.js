ec23838d5d27ad6112ec9cb29e0c4e9cab404048
gandori <marcel.stiebing@web.de>
2022-06-01 20:24:20 +0200

    add score and timer and fix bug

--- src/index.js
+++ src/index.js
@@ -1,79 +1,148 @@
-const body = document.querySelector("body");
+var body = document.querySelector("body");
 
-const board = document.getElementById("board");
-const board_size = 800;
+var board = document.getElementById("board");
+var board_size = 500;
 board.style.width = board_size +"px";
 board.style.height = board_size +"px";
 
-const snake = [];
-const snakehead = document.getElementById("snakehead");
-const snakehead_pos = {x:board_size/2, y:board_size/2}
-const snake_part_size = 40;
-snakehead.style.width = snake_part_size + "px";
-snakehead.style.height = snake_part_size + "px";
-snakehead.style.top = snakehead_pos.y + "px";
-snakehead.style.left = snakehead_pos.x + "px";
+var snake = [];
+var snakehead;
+var snakehead_pos;
+create_snakehead();
 
-const fruit_colors = ["red", "yellow", "purple", "green"];
-const user_input = {up_key:false, down_key:false, left_key:false, right_key:false}
+var fruit_colors = ["red", "yellow", "purple", "green"];
+var user_input = {up_key:false, down_key:false, left_key:false, right_key:false}
 
+var score = 0;
+var time = [0,0,0];
 var last_direction;
 var fruit = true;
-var game_speed = 1000/7;
+var game_speed = 1000/5;
+var snake_part_size;
 var snake_speed = snake_part_size;
+
+var game_interval;
+var time_interval;
 
 function main()
 {
-    document.getElementById("startpage").style.display = "none";
-    board.style.display = "block";
+    var startpage = document.getElementById("startpage")
+    startpage.style.transition = "margin 1s";
+    startpage.style.marginLeft = "-5000px";
 
-    //set user input
-    document.addEventListener('keydown', (e) =>{
-        const keycodes = [37, 38, 39, 40];
-        if(keycodes.includes(e.keyCode) == true)
-        {
-            for(key in user_input){user_input[key] = false;};
-        };
-        switch(e.keyCode) {
-            case 37: //left key
-            	if(last_direction==39){user_input.right_key=true;break;};
-  			user_input.left_key = true;last_direction=37;break;
-            case 38: //up key
-            	if(last_direction==40){user_input.down_key=true;break;};
-            	user_input.up_key=true;last_direction=38;break;
-            case 39: //right key
-            	if(last_direction==37){user_input.left_key=true;break;};
-            	user_input.right_key=true;last_direction=39;break;
-            case 40: //down key
-            	if(last_direction==38){user_input.up_key=true;break;};
-            	user_input.down_key=true;last_direction=40;break;
-	   };
-    });
+    var board_container = document.getElementById("board_container");
+    board_container.style.transition = "margin 1s";
+    board_container.style.marginLeft = "0px";
 
-	//random start direction
-	if(Math.random() < 0.5 == true){user_input.left_key=true;last_direction=37;}
-	else if (Math.random() < 0.5 == true){user_input.up_key=true;last_direction=38;}
-	else if (Math.random() < 0.5 == true){user_input.right_key=true;last_direction=39;}
-	else if (Math.random() < 0.5 == true){user_input.down_key=true;last_direction=40;}
-	else{user_input.up_key = true;};
+    setTimeout(() => {
+       game_loop();
+    }, 2000);
+};
 
-	//create first snake part
-	create_new_snake_part()
-
-    //mainloop
-    interval = setInterval(function()
-    {
+function game_loop()
+{
+    document.addEventListener("keydown", input =>test(input));
+    random_start_direction();
+    game_interval = setInterval(() => {
         let last_pos = move_snakehead();
         move_snake_parts(last_pos);
         spawn_fruit();
         snakehead_on_fruit_position();
-    }, game_speed);
+        increase_score();           
+    }, game_speed);    
+    time_interval = setInterval(increase_time, 1000/60);
+};
+
+function test(input)
+{
+    var e = input;
+    var keycodes = [37, 38, 39, 40];
+    if(keycodes.includes(e.keyCode) == true)
+    {
+        for(key in user_input){user_input[key] = false;};
+    };
+    switch(e.keyCode) {
+        case 37: //left key
+            if(last_direction==39){user_input.right_key=true;break;};
+          user_input.left_key = true;last_direction=37;break;
+        case 38: //up key
+            if(last_direction==40){user_input.down_key=true;break;};
+            user_input.up_key=true;last_direction=38;break;
+        case 39: //right key
+            if(last_direction==37){user_input.left_key=true;break;};
+            user_input.right_key=true;last_direction=39;break;
+        case 40: //down key
+            if(last_direction==38){user_input.up_key=true;break;};
+            user_input.down_key=true;last_direction=40;break;
+   };
+};
+
+function create_snakehead()
+{
+    snakehead = document.createElement("div");
+    snakehead.setAttribute("id","snakehead");
+    snake_part_size = board_size/20;
+    snakehead_pos = {x:board_size/2, y:board_size/2}
+    snakehead.style.width = snake_part_size + "px";
+    snakehead.style.height = snake_part_size + "px";
+    snakehead.style.top = snakehead_pos.y + "px";
+    snakehead.style.left = snakehead_pos.x + "px";
+    snakehead.style.position = "absolute";
+    snakehead.style.backgroundColor = "rgb(100,100,200)";
+    snakehead.style.zIndex = "3";
+    board.append(snakehead);
+};
+
+function increase_score()
+{
+    var label = document.getElementById("score");
+    label.innerHTML = "Score: " + score;
+};
+
+function increase_time()
+{
+    var label = document.getElementById("time");
+    time[2]++;
+    if(time[2] == 60)
+    {
+        time[2] = 0;
+        time[1]++;
+    };
+    if(time[1] == 60)
+    {
+        time[1] = 0;
+        time[0]++;
+    };
+    label.innerHTML = "Score: " + time.toString().replace(",", ".").replace(",", ".");
+};
+
+function random_start_direction()
+{
+    var rng = Math.floor(Math.random() *4) + 1;
+    switch(rng)
+    {
+        case 1:
+            user_input.left_key=true;
+            last_direction=37;
+            break;
+        case 2:
+            user_input.up_key=true;
+            last_direction=38;
+            break;
+        case 3:
+            user_input.right_key=true;
+            last_direction=39;
+            break;
+        case 4:
+            user_input.down_key=true;
+            last_direction=40;
+            break;
+    };
 };
 
 function move_snakehead()
 {
     let pos = {x:snakehead_pos.x, y:snakehead_pos.y}
-
     if(user_input.left_key == true)
     {
         snakehead_pos.x = snakehead_pos.x - snake_speed;
@@ -150,13 +219,17 @@ function snakehead_on_fruit_position()
         fruit.remove();
         fruit = true;
         create_new_snake_part();
+        score++;
     };
 };
 
 function create_new_snake_part()
 {
-    snake_part = document.createElement("div");
+    let snake_part = document.createElement("div");
     snake_part.setAttribute("class","snake");
+    snake_part.style.backgroundColor = "rgb(200,200,200)";
+    snake_part.style.position = "absolute";
+    snake_part.style.zIndex = "3";
     snake_part.style.width = snake_part_size +"px";
     snake_part.style.height = snake_part_size +"px";
     snake_part.style.top = snakehead_pos.y + "px";
@@ -186,15 +259,41 @@ function death()
         if(snakehead_pos.y == y && snakehead_pos.x == x) a = true;
     });
 
-    if(a == true){create_message(); clearInterval(interval);};
+    if(a == true)
+    {
+        create_message(); 
+        clearInterval(game_interval);
+        clearInterval(time_interval)
+    };
 };
 
 function create_message()
 {
-    let message = document.createElement("h1");
-    message.innerHTML = "You Lose";
-    message.style.color = "red";
-    message.style.position = "absolute";
-    message.style.fontFamily = "monospace";
-    body.append(message);
+    let msg_container = document.createElement("div");
+    msg_container.setAttribute("id", "msg_container");
+
+    body.append(msg_container);
+
+    let msg = document.createElement("h1");
+    msg.innerHTML = "You Lose";
+    msg_container.append(msg);
+
+    let btn = document.createElement("button");
+    btn.innerHTML = "Retry";
+    btn.setAttribute("onclick", "window.location.reload()");
+    msg_container.append(btn);
+};
+
+function retry()
+{
+    score = 0;
+    time = [0,0,0];
+    clearInterval(game_interval);
+    clearInterval(time_interval)
+    document.getElementById("msg_container").remove();
+    game_loop();
+    while(board.firstChild)
+    {
+        board.firstChild.remove();
+    };
 };
